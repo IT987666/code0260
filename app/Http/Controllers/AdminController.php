139b44e8a4301f3da50ed481675eff2433c7836f
Omar <omarhamolaila8@gmail.com>
2025-01-15 17:27:12 +0300

    edit

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -357,7 +357,7 @@ class AdminController extends Controller
 
 
 
-    public function order_details($order_id,$rowId)
+    public function order_details($order_id)
     {
         $order = Order::find($order_id);
 
